5a1d4b7346e93554a687b495f41a1256e84952b0
Sybille Kasslatter <sybille.kasslatter@hotmail.com>
2020-04-09 14:46:06 +0200

    minor changes to profile

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Homepage from "./pages/Homepage";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import Marketplace from "./pages/Marketplace";
 
 
 class App extends React.Component {
@@ -24,6 +25,8 @@ class App extends React.Component {
         <Route path="/signup" component={Signup}/>
         <Route path="/login" component={Login}/>
         <Route path="/profile" component={Profile}/>
+        <Route path="/marketplace" component={Marketplace}/>
+
       </Switch>
       <Footer />
     </div>
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { getUser } from '../utils/auth'
 
 class Profile extends Component {
 
   render() {
+    let user =getUser()
     return (
       <div>
-        <h1>It works woop woop!</h1>
+        <h1>Welcome {user.username}</h1>
+        <div>
+        <p>This are the plant you are offering at the moment:</p>
+
+          <ul>
+          <li>Plant 1</li>
+          <li>Plant 2</li>
+
+          </ul>
+        </div>
       </div>
     )
   }
