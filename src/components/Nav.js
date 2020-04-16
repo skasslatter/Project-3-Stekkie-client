f22345e39c14f7533c6e4801132fddba21c6fcc3
Yvana Wagter <yvana@wagter.nl>
2020-04-16 16:02:28 +0200

    nav bar/logout working

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -20,78 +20,38 @@ class Navigation extends React.Component {
     let user = getUser();
     return (
       <div className="navbar">
-      {!user ? (
-      <Navbar collapseOnSelect expand="lg"  variant="dark">
-        <Navbar.Brand href="/">
-        <img src="/Stekkie_logo-diagonal.png" className="logo-navbar"/>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link className="nav-link" href="/signup">Signup</Nav.Link>
-            <Nav.Link className="nav-link" href="/login">Login</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      
-      ) : (
-        <Navbar collapseOnSelect expand="lg"  variant="dark">
-        <Navbar.Brand href="/profile">
-        <img src="/Stekkie_logo-diagonal.png" className="logo-navbar"/>
-        </Navbar.Brand>        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link className="nav-link" href="/profile">Profile</Nav.Link>
-            <Nav.Link className="nav-link" href="/marketplace">Marketplace</Nav.Link>
-            <Nav.Link className="nav-link" href="/" onClick={this.logoutUser} className="logout">Logout</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+        {!user ? (
+          <Navbar collapseOnSelect expand="lg" variant="dark">
+            <Navbar.Brand href="/">
+              <img src="/Stekkie_logo-diagonal.png" className="logo-navbar" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/signup">Signup</Nav.Link>
+                <Nav.Link href="/login">Login</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
 
-      // <div>
-      //   {!user ? (
-      //     <div className="nav">
-      //       <div>
-      //         <Link to="/signup">Signup</Link>
-      //       </div>
-      //       <div>
-      //         <Link to="/">
-      //           <img
-      //             className="home-icon"
-      //             src="/pngkey.com-home-png-2373015 copy.png"
-      //             alt="Home"
-      //           ></img>
-      //         </Link>
-      //       </div>
-      //       <div>
-      //         <Link to="/login">Login</Link>
-      //       </div>
-      //     </div>
-      //   ) : (
-      //     <div className="nav">
-      //       <div>
-      //         <p>Welcome {user.username}</p>
-      //       </div>
-      //       <div>
-      //         <Link to="/profile">
-      //           <img
-      //             className="home-icon"
-      //             src="/pngkey.com-home-png-2373015 copy.png"
-      //             alt="Home"
-      //           ></img>
-      //         </Link>
-      //       </div>
-      //       <div>
-      //         <p onClick={this.logoutUser}>Logout</p>
-      //       </div>
-      //     </div>
-      //   )}
-      // </div>
+        ) : (
+            <Navbar collapseOnSelect expand="lg" variant="dark">
+              <Navbar.Brand href="/profile">
+                <img src="/Stekkie_logo-diagonal.png" className="logo-navbar" />
+              </Navbar.Brand>        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="/profile">Profile</Nav.Link>
+                  <Nav.Link href="/marketplace">Marketplace</Nav.Link>
+                  <p onClick={this.logoutUser} className="logout">Logout</p>
+                  </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       
-    )
-      }</div>
-    )
-  }
+    )}
+    </div>
+          )
+        }
 }
 
 export default withRouter(Navigation);
