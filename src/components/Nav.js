b1cfc0d317e077f702ce42b2f880173c27f836d7
Sybille Kasslatter <sybille.kasslatter@hotmail.com>
2020-04-16 17:42:54 +0200

    Nav centered

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -33,25 +33,26 @@ class Navigation extends React.Component {
               </Nav>
             </Navbar.Collapse>
           </Navbar>
-
         ) : (
-            <Navbar collapseOnSelect expand="lg" variant="dark">
-              <Navbar.Brand href="/profile">
-                <img src="/Stekkie_logo-diagonal.png" className="logo-navbar" />
-              </Navbar.Brand>        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                  <Nav.Link href="/profile">Profile</Nav.Link>
-                  <Nav.Link href="/marketplace">Marketplace</Nav.Link>
-                  <p onClick={this.logoutUser} className="logout">Logout</p>
-                  </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      
-    )}
-    </div>
-          )
-        }
+          <Navbar collapseOnSelect expand="lg" variant="dark">
+            <Navbar.Brand href="/profile">
+              <img src="/Stekkie_logo-diagonal.png" className="logo-navbar" />
+            </Navbar.Brand>{" "}
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/profile">Profile</Nav.Link>
+                <Nav.Link href="/marketplace">Marketplace</Nav.Link>
+                <p onClick={this.logoutUser} className="logout">
+                  Logout
+                </p>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        )}
+      </div>
+    );
+  }
 }
 
 export default withRouter(Navigation);
