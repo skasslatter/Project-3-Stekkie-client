fb0031e4c9eb93746d1720cada7d46a77d22fb52
Sybille Kasslatter <sybille.kasslatter@hotmail.com>
2020-04-16 17:30:01 +0200

    Auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 import qs from "qs";
 
 const axios = Axios.create({
-  baseURL: `${process.env.REACT_APP_API_BASE}`,
+  baseURL: `${process.env.REACT_APP_API_BASE}/`,
   withCredentials: true,
   //headers: {"content-type": "application/x-www-form-urlencoded"}
 });
