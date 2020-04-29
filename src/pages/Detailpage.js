b735c5921a672831667c8533fdbdf353c43e756c
Yvana Wagter <yvana@wagter.nl>
2020-04-30 00:14:39 +0200

    inbox is working but basic

--- src/pages/Detailpage.js
+++ src/pages/Detailpage.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Axios from "axios";
 import DetailView from "../components/DetailView";
+import qs from "querystring";
+import { getUser } from "../utils/auth";
+
 
 export default class DetailPage extends React.Component {
   constructor() {
     super();
     this.state = {
       plant: "",
-      apiInfo: ""
-    };
+      apiInfo: "",
+      messages: {
+        title: "",
+        message_to_user: "",
+        message_from: "",
+        message: "",
+      }
+    }
+    this.messageHandler = this.messageHandler.bind(this)
+    this.sendMessage = this.sendMessage.bind(this)
   }
 
   componentDidMount() {
@@ -20,13 +31,43 @@ export default class DetailPage extends React.Component {
     })
       .then((response) => {
         console.log(response);
-        this.setState({ plant: response.data.plant, apiInfo: response.data.api});
+        this.setState({ plant: response.data.plant, apiInfo: response.data.api });
       })
       .catch((error) => {
         console.log(error);
       });
   }
-
+  messageHandler(event){
+    let user = getUser()
+    let plant_owner = this.state.plant.creator
+    let plant_title = this.state.plant.title
+    this.setState({
+      messages: {
+        title: plant_title,
+        message_to_user: plant_owner,
+        message_from: user._id,
+        message: event.target.value
+      }
+    })
+  }
+  sendMessage(){
+    Axios({
+      method: "POST",
+      url: `${process.env.REACT_APP_API_BASE}/messages/newmessage`,
+      withCredentials: true,
+      data: qs.stringify(this.state.messages),
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      }
+    })
+      .then((response) => {
+        console.log(response)
+        this.props.history.push("/profile");
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
   render() {
     return (
       <div>
@@ -34,8 +75,13 @@ export default class DetailPage extends React.Component {
         {this.state.plant && 
         <DetailView 
         plant={this.state.plant} 
-        api={this.state.apiInfo} 
+        api={this.state.apiInfo}
         />}
+        <div>
+          <h3>Leave a message for the plant owner</h3>
+          <textarea name="" cols="40" rows="10" onChange={this.messageHandler} placeholder="Type your message to the lovely plant owner here and exchange your contact information to swap"></textarea>
+          <button onClick={this.sendMessage}>Start swapping!</button>
+        </div>
       </div>
     );
   }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,24 +4,55 @@ import Axios from "axios";
 import { Redirect, withRouter } from "react-router-dom";
 import { Link } from "react-router-dom";
 import PlantCard from "../components/PlantCard";
+import "../stylesheets/profile.css";
 
 class Profile extends Component {
   constructor() {
     super();
     this.state = {
       userPlants: [],
+      messages: {
+        title: "",
+        all_messages: [],
+        sender_of_message: "",
+        email_sender: ""
+      }
     };
   }
 
   componentDidMount() {
+    let user = getUser()
     Axios({
       method: "GET",
       url: `${process.env.REACT_APP_API_BASE}/userPlants`,
-      withCredentials: true,
+      withCredentials: true
     })
-      .then((response) => {
-        console.log(response.data);
-        this.setState({ userPlants: response.data.userPlants });
+      .then((responsePlants) => {
+        Axios({
+          method: "GET",
+          url: `${process.env.REACT_APP_API_BASE}/messages/${user._id}`,
+          withCredentials: true
+        })
+          .then((responseMessages) => {
+            if (responseMessages.data.messages.length === 0){
+              this.setState({
+                userPlants: responsePlants.data.userPlants
+              })
+            } else {
+              this.setState({
+                userPlants: responsePlants.data.userPlants,
+                messages: {
+                  title: responseMessages.data.messages[0].title,
+                  all_messages: [responseMessages.data.messages[0].message],
+                  sender_of_message: responseMessages.data.messages[0].message_from.username,
+                  email_sender: responseMessages.data.messages[0].message_from.email
+                }
+              });
+            } 
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       })
       .catch((err) => {
         console.log(err);
@@ -48,42 +79,52 @@ class Profile extends Component {
   render() {
     let user = getUser();
     let userPlants = this.state.userPlants;
+    let messages = this.state.messages.all_messages;
     return (
       <div>
         {!user ? (
           <Redirect to="/login" />
         ) : (
-          <div>
-            <div className="hero-image-profile">
-              <div className="hero-text">
+            <div>
+              <div className="hero-image-profile">
+                <div className="hero-text">
                   <h1>Welcome {user.username}!</h1>{" "}
+                </div>
               </div>
-            </div>
 
-            <div className="container">
-              <h4>The plants you want to share:</h4>
+              <div className="container">
+                <h4>The plants you want to share:</h4>
 
-              <div className="card-columns">
-                {userPlants.map((plant, index) => {
-                  return (
-                    <PlantCard
-                      key={index}
-                      plant={plant}
-                      onDelete={() => {
-                        this.deletePlant(plant._id);
-                      }}
-                    />
-                  );
-                })}
-              </div>
-              <div>
-                <Link to="/add-plant" className="caption btn btn-success">
-                  Add your plants
+                <div className="card-columns">
+                  {userPlants.map((plant, index) => {
+                    return (
+                      <PlantCard
+                        key={index}
+                        plant={plant}
+                        onDelete={() => {
+                          this.deletePlant(plant._id);
+                        }}
+                      />
+                    );
+                  })}
+                </div>
+                <div>
+                  <Link to="/add-plant" className="caption btn btn-success">
+                    Add your plants
                 </Link>
+                </div>
+                <div className="message-inbox">
+                  <h4>Messages you received from other plant owners</h4>
+                  {messages !== null && messages.map((message, index) => (
+                    <div className="message">
+                      <h6>{this.state.messages.title}</h6>
+                      <p key={index}>{message}</p><p> from {this.state.messages.sender_of_message} - {this.state.messages.email_sender}</p>
+                      </div>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
       </div>
     );
   }
